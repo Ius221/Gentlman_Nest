3388b99495b1d03a3938cb4a8866b3343c184ec8
Ayush Gupta <ayushkumar9783@gmail.com>
2026-04-15 23:56:44 +0530

    signin | signup api done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,22 @@
+import { UserDto } from './../users/dtos/user.dto';
 import { AuthService } from './auth.service';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { Serialize } from 'src/interceptor/serialize.interceptor';
+import { SigninUserDto } from './dtos/signin-user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  signupNewUser(@Body() createUserDto: CreateUserDto) {
+  createNewUser(@Body() createUserDto: CreateUserDto) {
     return this.authService.createNewUser(createUserDto);
+  }
+
+  @Post('signin')
+  signinUser(@Body() signinUserDto: SigninUserDto) {
+    return this.authService.signinUser(signinUserDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,52 @@
+import { SigninUserDto } from './dtos/signin-user.dto';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './../users/users.service';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { randomBytes, scrypt } from 'crypto';
+import { promisify } from 'util';
+import { User } from '../users/user.entity';
+
+const myScrypt = promisify(scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
-  async createNewUser(createUserDto: CreateUserDto) {
+  async createNewUser(createUserDto: CreateUserDto): Promise<User> {
+    const user = await this.usersService.findByEmail(createUserDto.email);
+    if (user) throw new BadRequestException('Email already exists');
+
+    const salt = randomBytes(8).toString('hex');
+    const generatedHash = (await myScrypt(
+      createUserDto.password,
+      salt,
+      32,
+    )) as Buffer;
+
+    createUserDto.password = salt + '.' + generatedHash.toString('hex');
+
     return await this.usersService.createNewUser(createUserDto);
+  }
+
+  async signinUser(signinUserDto: SigninUserDto) {
+    const user = await this.usersService.findByEmail(signinUserDto.email);
+    if (!user) throw new UnauthorizedException('Invalid Credentials');
+
+    const [salt, hashed] = user.password.split('.');
+    const generatedHash = (await myScrypt(
+      signinUserDto.password,
+      salt,
+      32,
+    )) as Buffer;
+
+    if (hashed !== generatedHash.toString('hex'))
+      throw new UnauthorizedException('Invalid Credentials');
+
+    return user;
+    // return await this.usersService;
   }
 }
